42060c3477b958aa4dc46d63195e7364b7b6024c
Roni <josim.uddin@bjitgroup.com>
2024-08-30 16:49:57 +0600

    demo class added

---
+++ src/main/java/com/example/demo/roni/DemoClass.java
@@ -0,0 +1,10 @@
+package com.example.demo.roni;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DemoClass {
+    public void DemoMethod() {
+        System.out.println("Hello World");
+    }
+}
